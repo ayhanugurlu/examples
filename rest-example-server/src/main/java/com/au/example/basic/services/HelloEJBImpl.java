cbc425795fdf80db04d018e56bd92c9b8c4dfc79
ayhanu <ayhanu@ayhanu-linux.ayesas.com>
2016-06-06 15:48:54 +0300

    interceptor log example

--- rest-example-server/src/main/java/com/au/example/basic/services/HelloEJBImpl.java
+++ rest-example-server/src/main/java/com/au/example/basic/services/HelloEJBImpl.java
@@ -1,5 +1,11 @@
 package com.au.example.basic.services;
 
+import javax.ejb.Stateless;
+
+import com.au.example.interceptor.Log;
+
+@Log
+@Stateless
 public class HelloEJBImpl implements HelloEJB {
 	public String sayHelloEJB(String name) {
 		return "Hello " + name;
